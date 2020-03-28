e1027e276452f27d3ed053db76dc3c84443bca02
ev001 <cho.evergreen@gmail.com>
2020-03-28 20:45:18 +0900

    web 패키지 위치 수정

--- src/test/java/com/dennis/book/springboot/HelloControllerTest.java
+++ src/test/java/com/dennis/book/springboot/HelloControllerTest.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import web.HelloController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
